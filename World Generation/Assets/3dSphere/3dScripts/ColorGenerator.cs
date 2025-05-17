b54e4f6f2546c27e572bbc23d459b01b7015f4ab
ArcCreate <arckumar12@gmail.com>
2025-05-16 17:49:17 -0700

    --

--- World Generation/Assets/3dSphere/3dScripts/ColorGenerator.cs
+++ World Generation/Assets/3dSphere/3dScripts/ColorGenerator.cs
@@ -47,6 +47,22 @@ public class ColorGenerator
         steepTexture.Apply();
         settings.planetMaterial.SetTexture("_CliffTexture", steepTexture);
         settings.planetMaterial.SetFloat("_SteepnessThreshold", settings.steepnessThreshold);
-        settings.planetMaterial.SetFloat("_SteepnessBlending", settings.steepnessBlend);        
+        settings.planetMaterial.SetFloat("_SteepnessBlending", settings.steepnessBlend);
+
+        //new steepness
+        settings.planetMaterial.SetFloat("_GrassMaxElevation", settings.grassMaxElevation);
+        settings.planetMaterial.SetFloat("_FlatSteepnessThreshold", settings.flatSteepnessThreshold);
+        settings.planetMaterial.SetFloat("_RockySteepnessThreshold", settings.rockySteepnessThreshold);
+
+        settings.planetMaterial.SetFloat("_ElevationLowThreshold", settings.elevationLowThreshold);
+        settings.planetMaterial.SetFloat("_ElevationHighThreshold", settings.elevationHighThreshold);
+
+        settings.planetMaterial.SetColor("_SandColor", settings.sandColor);
+        settings.planetMaterial.SetColor("_RockyLowColor", settings.rockyLowColor);
+        settings.planetMaterial.SetColor("_RockyHighColor", settings.rockyHighColor);
+        settings.planetMaterial.SetColor("_GrassA", settings.grassA);
+        settings.planetMaterial.SetColor("_GrassB", settings.grassB);
+        settings.planetMaterial.SetColor("_GrassC", settings.grassC);
+        settings.planetMaterial.SetColor("_GrassD", settings.grassD);
     }
 }
--- World Generation/Assets/3dSphere/3dScripts/ColorSettings.cs
+++ World Generation/Assets/3dSphere/3dScripts/ColorSettings.cs
@@ -13,4 +13,23 @@ public class ColorSettings : ScriptableObject
     public float steepnessThreshold = 0.5f;
     [Range(0f, 1f)]
     public float steepnessBlend = 0.5f;
+
+    //new elevation colors
+    // Color settings
+    public Color sandColor;
+    public Color rockyLowColor;
+    public Color rockyHighColor;
+
+    public Color grassA;
+    public Color grassB;
+    public Color grassC;
+    public Color grassD;
+
+    // Control parameters
+    [Range(0, 1)] public float grassMaxElevation = 0.6f;
+    [Range(0, 1)] public float flatSteepnessThreshold = 0.3f;
+    [Range(0, 1)] public float rockySteepnessThreshold = 0.6f;
+
+    [Range(0, 1)] public float elevationLowThreshold = 0.3f;
+    [Range(0, 1)] public float elevationHighThreshold = 0.7f;
 }
